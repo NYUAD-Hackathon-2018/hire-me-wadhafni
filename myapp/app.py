6905b81a199357050cea75c8e65e28fc38dcad92
ShanJ35 <sj2538@nyu.edu>
2018-04-28 18:01:01 +0400

    remote server data pulling

--- myapp/app.py
+++ myapp/app.py
@@ -2,8 +2,9 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for, flash
 from pymongo import MongoClient
 
+
 #intializes MongoDB
-client = MongoClient('localhost', 27017)
+client = MongoClient("mongodb+srv://hackathon2018:Shantanu123$%^@nyuadwadhafni2018-sjngs.mongodb.net/test")
 #select database
 db = client.directory
 #select collection within that database
@@ -17,11 +18,15 @@ def get_worker():
 	req_location = request.args.get('loc')
 	req_skill = request.args.get('skill')
 	req_language = request.args.get('lang')
-	collection_list = collection.find({"skill": req_skill, "loc": req_location, "status": "true"}, {"_id": False})
+	collection_list = collection.find({"skill": req_skill, "loc": req_location, "status": "true", "language": req_language}, {"_id": False})
 	worker_list = []
 	count = 0
 	for worker in collection_list:
 		worker = jsonify(worker)
 		worker_list.append(worker)
 		count = count + 1
+
 	return worker_list[0]
+
+if __name__ == "__main__":
+   app.run(host="0.0.0.0", port=8080)
