11d9b5c2ce2a3cdc67c0e3e6196f81a7390854e8
pecoraa <elena.l@hotmail.lt>
2018-04-28 16:11:14 +0400

    add job listings page

--- mclient/src/app/app.module.ts
+++ mclient/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { SearchPage } from '../pages/search/search';
+import { JobsPage } from '../pages/jobs/jobs';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    SearchPage
+    SearchPage,
+    JobsPage
   ],
   imports: [
     BrowserModule,
@@ -22,7 +24,8 @@ import { SearchPage } from '../pages/search/search';
   entryComponents: [
     MyApp,
     HomePage,
-    SearchPage
+    SearchPage,
+    JobsPage
   ],
   providers: [
     StatusBar,
--- mclient/src/pages/search/search.module.ts
+++ mclient/src/pages/search/search.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SearchPage } from './search';
+import { JobsPage } from '../../pages/jobs/jobs';
 
 @NgModule({
   declarations: [
     SearchPage,
+    JobsPage
   ],
   imports: [
     IonicPageModule.forChild(SearchPage),
