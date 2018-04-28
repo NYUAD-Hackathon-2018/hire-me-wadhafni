f3fe701ea8ef3006a472d000537ad726041fa0a2
ShanJ <sj2538@nyu.edu>
2018-04-28 14:43:09 +0400

    database

--- myapp/app.py
+++ myapp/app.py
@@ -1,16 +1,30 @@
-from flask import Flask, jsonify
+#Needs flask, flask-pymongo
+from flask import Flask, jsonify, render_template, request, redirect, url_for
+from pymongo import MongoClient
+from bson.json_util import dumps, loads
 
-
-person1 = {"phone": "888888", "name":"nyu", "loc": "dubai", "skill":"bakery"}
-data = [person1]
-
+#intializes MongoDB
+client = MongoClient('localhost', 27017)
+#select database
+db = client.directory
+#select collection within that database
+collection = db.inventory
 # def create_app():
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "hello world"
 
-@app.route("/getWorkers")
+#def hello_world():
+  #  return "hello world"
+
+#@app.route("/getWorkers")
+@app.route("/")
 def get_worker():
-    return jsonify(data)
+    #get the object based on search parameter
+    collection_list = collection.find({"status": "true"}, {"_id": False})
+    worker_list = []
+    for worker in collection_list:
+    	worker_list.append(worker)
+
+    return jsonify(worker_list[0])
+    #return some info of the search result
+    
